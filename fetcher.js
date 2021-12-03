08578af5d8c49296200e2bec07e74c9400e605d7
Sahar <saharhalim151@gmail.com>
2021-12-03 00:04:19 +0000

    first commit

---
+++ fetcher.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const path = process.argv[3];
+
+console.log('url: ', url);
+console.log('path: ',path);
+
+request('http://example.edu', (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  const bytes = body.length;
+  fs.writeFile(path, body, err => {
+    if (err) {
+      console.error(err)
+      return
+    } console.log(`Downloaded and saved ${bytes} bytes to ${path}`)
+    //file written successfully
+  })
+});
+
+
+
